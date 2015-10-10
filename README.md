22e9873c37cfe7727addccbce38f57053ca13777
Saiya <leesaiya@Jo.local>
2015-10-10 23:40:18 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+evecalm.github.com
+==================
+
+Saiya's app site. Please visit http://app.evecalm.com
---
+++ js/ie9.js
@@ -0,0 +1,2 @@
+var message;message="<h1>你正在使用过时的浏览器, 请升级浏览器后再浏览本站</h1>\n<p>请升级至IE10及以上版本的IE浏览器, 或者选用 Chrome/Firefox, 如果你正在使用国产双核浏览器, 请切换至急速模式.</p>\n<p>换掉老旧过时的浏览器, 你可以拥有更快速, 更更丰富精彩及更安全的上网体验. 并有助于科技进步, 并减少, 减少相关公司/企业/政府的支出, 促进经济进步. 我不是在逗你玩 :) </p>",document.documentElement.className+=" ie9",document.write(message);
+//# sourceMappingURL=./ie9.js.map
