289c002dcd37ea26c99944611db1a7b6a91728e3
Saiya <leesaiya@Jo.local>
2015-10-23 18:10:59 +0800

    added description and keywords info

--- js/index.js
+++ js/index.js
@@ -1,1 +1,1 @@
-"evecalm.com"!==location.hostname&&(location.href="http://evecalm.com"),"ontouchstart"in window&&(document.documentElement.className+=" touch-device");
+"ontouchstart"in window&&(document.documentElement.className+=" touch-device");
