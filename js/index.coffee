ca42ae807ab7c3eccff27b2ab49435167b4571e8
Saiya <leesaiya@Jo.local>
2015-10-23 18:08:56 +0800

    added description and keywords

--- js/index.coffee
+++ js/index.coffee
@@ -1,6 +1,2 @@
-
-if location.hostname isnt 'evecalm.com'
-  location.href = 'http://evecalm.com'
-
 if 'ontouchstart' of window
   document.documentElement.className += ' touch-device'
--- js/index.js
+++ js/index.js
@@ -1,2 +1,2 @@
-"evecalm.com"!==location.hostname&&(location.href="http://evecalm.com"),"ontouchstart"in window&&(document.documentElement.className+=" touch-device");
+"ontouchstart"in window&&(document.documentElement.className+=" touch-device");
 //# sourceMappingURL=./index.js.map
